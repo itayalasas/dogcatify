37b2c857174be6e0655ea2fe7e516f6172c9fc93
itayalasas <info@ayalait.com.uy>
2025-07-31 01:12:37 -0300

    Updated AuthContext.tsx

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -148,6 +148,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         setLoading(false);
         setAuthInitialized(true);
       }
+      }
     );
 
     // Initial session check
